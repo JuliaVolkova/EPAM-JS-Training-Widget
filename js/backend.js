66e007c15d8a691dbf7fe2441f2dc9d13dee344d
Julia Volkova <julia.volkova789@gmail.com>
2017-11-04 23:09:19 +0300

    feat: Add module with xhr

---
+++ js/backend.js
@@ -0,0 +1,28 @@
+'use strict';
+
+window.backend = (function () {
+    return {
+        /**
+         * @param {String} city
+         * @param {String} code
+         * @param {Function} success
+         * @param {Function} error
+         */
+        load: function (city, code, success, error) {
+            var xhr = new XMLHttpRequest();
+            xhr.open('get', 'https://api.openweathermap.org/data/2.5/forecast?q=' + city + ',' + code + '&APPID=1ce776c63fa744abd79089ebdecbf860');
+            xhr.addEventListener('load', function () {
+                if (xhr.status === 200) {
+                    var data = JSON.parse(xhr.responseText);
+                    success(data);
+                } else {
+                    error();
+                }
+            });
+
+            xhr.addEventListener('error', error);
+
+            xhr.send();
+        }
+    };
+})();
